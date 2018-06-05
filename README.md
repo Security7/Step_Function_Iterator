11c829d51945acbd7c91fca9a678cec269e71d74
David Gatti <github@gatti.pl>
2018-06-05 22:52:12 +0000

    First code

---
+++ README.md
@@ -0,0 +1,7 @@
+# Step Function Iterator
+
+This repository is used in conjunction with a step function to count how many times a Step Function loop occurred; since a SF can only have 25,000 events in its logs 
+
+# More about us
+
+[Security7](https://www.security7.net/) is a group of security professionals that strive to make security: affordable, scalable and reliable. If you'd like to get in touch with use, go to our [contact](https://www.security7.net/contact/) page.
--- index.js
+++ index.js
@@ -1,4 +1,28 @@
+//
+//	This step function is responsabile for incrementing our count so we know
+//	how many time the Step Function looped on itself
+//
 exports.handler = async (event) => {
-    // TODO implement
-    return 'Hello from Lambda!'
+	
+	//
+	//	1.	Get all the information that is passed between the Lambda functions
+	//		and save them in clear variables
+	//
+	let loop_limit = event.loop_limit;
+	let loop_count = event.loop_count;
+	
+	//
+	//	2. Increment our counter
+	//
+	loop_count++;
+  
+	//
+	//	->	Retunr all the unchanged and changed data to the next step
+	//
+	 return {
+		loop_limit: loop_limit,
+		loop_count: loop_count,
+		count_limit: loop_count < loop_limit
+	};
+	
 };
