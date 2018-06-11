ab11f9ec7061bb9904516601ef63745561c2d090
David Gatti <github@gatti.pl>
2018-06-11 18:50:12 +0000

    Updating data without removing it

--- index.js
+++ index.js
@@ -3,26 +3,23 @@
 //	how many time the Step Function looped on itself
 //
 exports.handler = async (event) => {
+
+	//
+	//	1.	Update the loop count so we can detect if we are exciding the 
+	//		ammount of loops that one Step Function can handle
+	//
+	event.loop_count++;
 	
 	//
-	//	1.	Get all the information that is passed between the Lambda functions
-	//		and save them in clear variables
+	//	2.	Check if the loop count excide the limit and if so we set the
+	//		variable to true, so Step Function can decide if it should 
+	//		call the Restart function or not.
 	//
-	let loop_limit = event.loop_limit;
-	let loop_count = event.loop_count;
-	
-	//
-	//	2. Increment our counter
-	//
-	loop_count++;
+	event.count_limit = event.loop_count < event.loop_limit;
   
 	//
 	//	->	Retunr all the unchanged and changed data to the next step
 	//
-	 return {
-		loop_limit: loop_limit,
-		loop_count: loop_count,
-		count_limit: loop_count < loop_limit
-	};
+	return event;
 	
 };
